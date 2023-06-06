74c161499e0c6d3dfa0d91490dc9e13ec10960cc
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-06-06 09:23:21 -0500

    feat: :sparkles: se arreglo el bug al borrar todas las compras
    tambien se agrego la funcionalidad al btn de mostrar o los valores de los impust

--- js/my-body.js
+++ js/my-body.js
@@ -1,79 +1,97 @@
 
 import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
-export class myBody extends HTMLElement{
-    constructor(){
-        super();
-        this.count = 2;
-        document.adoptedStyleSheets.push(styles);
-    }
-    async components(){
-        return await (await fetch("view/my-body.html")).text();
-    }
-
-    async add(e){
-        let $ = e.target;
-        if ($.nodeName == "BUTTON") {
-            let plantilla = await (await fetch("view/my-productDetails.html")).text();
-            let render = new DOMParser().parseFromString(plantilla, "text/html")["body"];
-            render.children[0].id="product_"+this.count;
-            let butons = render.querySelectorAll("button");
-            butons.forEach(element => {
-                element.dataset.row = "product_"+this.count;
-            });
-            document.querySelector("#products").insertAdjacentElement("beforeend", render.children[0]);
-            this.count++;
-        }
-    }
-    connectedCallback(){
-        this.components().then(html =>{
-            this.innerHTML = html;
-            this.add = this.querySelector("#btnadd").addEventListener("click", this.add.bind(this));
-        })
-    }
-}
-
-customElements.define("my-body" , myBody); 
-
-
-
-
-
-
-
-/*
-esta es la logica del profesor para que se guie
-/**
- * todo: Miguel del futuro arregle esto ▼ 
- * ? Esta una Mier.....da :)
- * @var styles
-*/
-/* import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
 export class myBody extends HTMLElement {
     constructor() {
         super();
-        this.count = 1;
         document.adoptedStyleSheets.push(styles);
     }
     async components() {
         return await (await fetch("view/my-body.html")).text();
     }
-    async add(){
-        let plantilla = await (await fetch("view/my-productDetails.html")).text();
-        let render = new DOMParser().parseFromString(plantilla, "text/html")["body"];
-        render.children[0].id="product_"+this.count;
-        let buttons = render.querySelectorAll("button");
-        buttons.forEach(element => {
-            element.dataset.row = "product_"+this.count;
+    async add(e){
+        let $ = e.target;
+        let cant = this.querySelector("#products").childElementCount;
+        if ($.nodeName == "BUTTON" && cant > 1 ) {
+            this.plantilla = this.querySelector("#products").children;
+            this.plantilla =  this.plantilla[ this.plantilla.length-1];
+            this.plantilla =  this.plantilla.cloneNode(true);
+            document.querySelector("#products").insertAdjacentElement("beforeend", this.plantilla);
+        }else{
+            document.querySelector("#products").insertAdjacentElement("beforeend", this.plantilla);
+        }
+    }
+
+    send(e){
+        let inputs = document.querySelectorAll("input");
+        
+    }
+
+
+
+    connectedCallback() {
+        this.components().then(html => {
+            this.innerHTML = html;
+            this.add = this.querySelector("#add").addEventListener("click", this.add.bind(this));
+            this.send = this.querySelector("#send").addEventListener("click", this.send.bind(this));
+        })
+    }
+}
+customElements.define('my-body', myBody);
+
+
+/* logica del profedor
+
+
+import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
+export class myBody extends HTMLElement {
+    constructor() {
+        super();
+        document.adoptedStyleSheets.push(styles);
+    }
+    async components() {
+        return await (await fetch("view/my-body.html")).text();
+    }
+    async add(e){
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            this.plantilla = this.querySelector("#products").children;
+            this.plantilla =  this.plantilla[ this.plantilla.length-1];
+            this.plantilla =  this.plantilla.cloneNode(true);
+            document.querySelector("#products").insertAdjacentElement("beforeend", this.plantilla);
+        }
+    }
+    send(e){
+        let input = document.querySelectorAll("input");
+        let info = {}, producto = {}, lista = {}, data = {}, count = 0;
+        producto.product = [];
+        input.forEach((val, id) => {
+            if (id <= 7) {
+                info[val.name] = val.value;
+            } else if (count <= 4) {
+                lista[val.name] = val.value;
+                count++;
+                if (count == 4) {
+                producto.product.push(lista);
+                lista = {};
+                count = 0;
+                }
+            }
         });
-        document.querySelector("#products").insertAdjacentElement("beforeend", render.children[0]);
-        this.count++;
+        data.info = info;
+        data.producto = producto.product;
+        console.log(data);
     }
     connectedCallback() {
         this.components().then(html => {
             this.innerHTML = html;
-            this.add = this.querySelector("#btnadd").addEventListener("click", this.add.bind(this));
+            this.add = this.querySelector("#add").addEventListener("click", this.add.bind(this));
+            this.send = this.querySelector("#send").addEventListener("click", this.send.bind(this));
         })
     }
 }
-customElements.define('my-body', myBody); */
+customElements.define('my-body', myBody);
 
+
+
+
+*/

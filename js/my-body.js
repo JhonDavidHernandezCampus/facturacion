5f928f89dc6f9cd57a55d4ebd263f1a7fcbfc4a8
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-06-07 07:30:16 -0500

    feat: :rocket: se agregaron losbos harchivos y se crearon las carpetas db y users

--- js/my-body.js
+++ js/my-body.js
@@ -10,49 +10,6 @@ export class myBody extends HTMLElement {
     }
     async add(e){
         let $ = e.target;
-        let cant = this.querySelector("#products").childElementCount;
-        if ($.nodeName == "BUTTON" && cant > 1 ) {
-            this.plantilla = this.querySelector("#products").children;
-            this.plantilla =  this.plantilla[ this.plantilla.length-1];
-            this.plantilla =  this.plantilla.cloneNode(true);
-            document.querySelector("#products").insertAdjacentElement("beforeend", this.plantilla);
-        }else{
-            document.querySelector("#products").insertAdjacentElement("beforeend", this.plantilla);
-        }
-    }
-
-    send(e){
-        let inputs = document.querySelectorAll("input");
-        
-    }
-
-
-
-    connectedCallback() {
-        this.components().then(html => {
-            this.innerHTML = html;
-            this.add = this.querySelector("#add").addEventListener("click", this.add.bind(this));
-            this.send = this.querySelector("#send").addEventListener("click", this.send.bind(this));
-        })
-    }
-}
-customElements.define('my-body', myBody);
-
-
-/* logica del profedor
-
-
-import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
-export class myBody extends HTMLElement {
-    constructor() {
-        super();
-        document.adoptedStyleSheets.push(styles);
-    }
-    async components() {
-        return await (await fetch("view/my-body.html")).text();
-    }
-    async add(e){
-        let $ = e.target;
         if ($.nodeName == "BUTTON") {
             this.plantilla = this.querySelector("#products").children;
             this.plantilla =  this.plantilla[ this.plantilla.length-1];
@@ -62,15 +19,16 @@ export class myBody extends HTMLElement {
     }
     send(e){
         let input = document.querySelectorAll("input");
+        let fromInput = 7, fromProduct = 4;
         let info = {}, producto = {}, lista = {}, data = {}, count = 0;
         producto.product = [];
         input.forEach((val, id) => {
-            if (id <= 7) {
+            if (id <= fromInput) {
                 info[val.name] = val.value;
-            } else if (count <= 4) {
+            } else if (count <= fromProduct) {
                 lista[val.name] = val.value;
                 count++;
-                if (count == 4) {
+                if (count == fromProduct) {
                 producto.product.push(lista);
                 lista = {};
                 count = 0;
@@ -79,7 +37,6 @@ export class myBody extends HTMLElement {
         });
         data.info = info;
         data.producto = producto.product;
-        console.log(data);
     }
     connectedCallback() {
         this.components().then(html => {
@@ -91,7 +48,3 @@ export class myBody extends HTMLElement {
 }
 customElements.define('my-body', myBody);
 
-
-
-
-*/
